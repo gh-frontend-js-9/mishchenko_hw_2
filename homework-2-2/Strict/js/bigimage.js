03830e86d5235624848c948a5f16424bd59df0b6
MaxMishchenko <dev.max.frontend.js@gmail.com>
2019-10-20 17:14:49 +0300

    Strict

---
+++ homework-2-2/Strict/js/bigimage.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('.minimized').click(function(event) {
+    var i_path = $(this).attr('src');
+    $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+    $('#magnify').css({
+    });
+    $('#overlay, #magnify').fadeIn('fast');
+  });
+  
+  $('body').on('click', '#close-popup, #overlay', function(event) {
+    event.preventDefault();
+ 
+    $('#overlay, #magnify').fadeOut('fast', function() {
+      $('#close-popup, #magnify, #overlay').remove();
+    });
+  });
+});
